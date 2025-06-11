beb9ec2fd94445afae7d3838571623d09556b57c
Jaxon Fryling <jaxon@Jaxons-MacBook-Air.local>
2025-06-10 17:48:00 -0700

    slight beginings of a UI overhall

--- frontend/src/pages/Projects.tsx
+++
@@ -1,7 +0,0 @@
-export default function Projects() {
-  return (
-    <div className="h-full flex items-center justify-center">
-      <h1 className="text-3xl font-bold">Projects</h1>
-    </div>
-  );
-}
